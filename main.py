829c782c319194d9374d8b0a1e57ccdf5af8a9ac
Fabian Castillo <fabian.cm@fcm-devops.xyz>
2021-12-15 21:17:55 -0600

    Day1

--- main.py
+++ main.py
@@ -4,6 +4,54 @@
 
 #print("Hello World!")
 
-print("Day 1 - Python Print Function", 
-"The function is declared like this:", 
-"print('what to print')")
+#1
+#print("Day 1 - Python Print Function", 
+#"The function is declared like this:", 
+#"print('what to print')")
+
+#Fix the code below 👇
+
+#2
+#print("Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#print('e.g. print("Hello " + "world")')
+#print("New lines can be created with a backslash and n.")
+
+#3
+#getlen = input ("What is your name? ") 
+#print(len(getlen))
+
+
+#4
+
+
+## 🚨 Don't change the code below 👇
+#a = input("a: ")
+#b = input("b: ")
+## 🚨 Don't change the code above 👆
+
+#####################################
+##Write your code below this line 👇
+#
+#g = a   
+#h = b 
+#
+#def my_function1():
+#    global a
+#    a = h
+#
+#my_function1()
+#
+#def my_function2():
+#    global b
+#    b = g
+#
+#my_function2()
+#
+##Write your code above this line 👆
+#####################################
+#
+## 🚨 Don't change the code below 👇
+#print("a: " + a)
+#print("b: " + b)
+#
