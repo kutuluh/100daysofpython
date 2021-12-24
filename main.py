f958e32a74a98fd8966323dea986750c53971151
Fabian Castillo <fabian.cm@fcm-devops.xyz>
2021-12-24 11:22:10 -0600

    day commit

---
+++ main.py
@@ -0,0 +1,50 @@
+## 🚨 Don't change the code below 👇
+#student_heights = input("Input a list of student heights ").split()
+#for n in range(0, len(student_heights)):
+#  student_heights[n] = int(student_heights[n])
+## 🚨 Don't change the code above 👆
+#
+#
+##Write your code below this row 👇
+#height_progress = 0
+#counter = 0
+#for student in student_heights:
+#    height_progress = height_progress + student
+#    counter = counter + 1
+#average = round(height_progress / counter)
+#print(average)
+
+# # 🚨 Don't change the code below 👇
+#student_scores = input("Input a list of student scores ").split()
+#for n in range(0, len(student_scores)):
+#  student_scores[n] = int(student_scores[n])
+#print(student_scores)
+## 🚨 Don't change the code above 👆
+#
+##Write your code below this row 👇
+#highest_score = 0
+#for student in student_scores:
+#    if student > highest_score:
+#        highest_score = student
+#print(f"The highest score in the class is: {highest_score}")        
+
+##Write your code below this row 👇
+#even_num_addition = 0
+#for number in range(1, 100+1):
+#    if (number%2 == 0):
+#        even_num_addition = even_num_addition + number
+#print(even_num_addition)        
+
+#Write your code below this row 👇
+#for number in range(1, 100+1):
+#    if (number%3==0) and (number%5==0):
+#        print("FizzBuzz")
+#    elif (number%3 ==0 ):
+#        print("Fizz")
+#    elif (number%5==0):
+#        print("Buzz")
+#    else:
+#        print(number)           
+#   
+
+
